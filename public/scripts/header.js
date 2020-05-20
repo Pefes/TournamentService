9bceac854815054041e6a0e84c4c88312f200a3c
Pefes <pawlo898@gmail.com>
2020-05-20 21:51:49 +0200

    add login popup menu

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,4 +1,4 @@
-document.getElementById("navButton").addEventListener("click", () => {
+document.getElementById( "navButton" ).addEventListener("click", () => {
     const navList = document.querySelector( "nav ul" ).cloneNode( true );
     const mobileDevContainer = document.getElementsByClassName( "mobileDevContainer" )[0];
 
@@ -12,5 +12,41 @@ document.getElementById("navButton").addEventListener("click", () => {
     document.getElementsByClassName( "mobileDevList" )[0].classList.toggle( "mobileDevListVisible" );
     document.querySelector( "nav" ).classList.toggle( "navShowMobileList" );
 });
+
+
+if( document.getElementsByClassName( "loginPopUp" )[0] ) {
+    document.getElementsByClassName( "loginPopUp" )[0].addEventListener("mouseover", () => {
+        const loginPopUpInfo = document.getElementsByClassName( "loginPopUpInfo" )[0];
+        const loginPopUpIcon = document.querySelector( ".loginPopUp i" );
+    
+        loginPopUpInfo.classList.add( "loginPopUpInfoHover" );
+        loginPopUpIcon.id = "loginPopUpIconHover";
+    });
+
+    document.getElementsByClassName( "loginPopUp" )[0].addEventListener("mouseleave", () => {
+        const loginPopUpInfo = document.getElementsByClassName( "loginPopUpInfo" )[0];
+        const loginPopUpIcon = document.querySelector( ".loginPopUp i" );
+    
+        loginPopUpInfo.classList.remove( "loginPopUpInfoHover" );
+        loginPopUpIcon.id = "";
+    });
+
+    const loginPopUpInfoA = document.querySelectorAll( ".loginPopUpInfo a" );
+
+    loginPopUpInfoA.forEach(aHref => {
+        aHref.addEventListener("mouseover", () => {
+            aHref.children[0].id = "loginPopUpInfoIconHover";
+        });
+        aHref.addEventListener("mouseleave", () => {
+            aHref.children[0].id = "";
+        });
+    });
+}
+
+
+
+
+
+
 
 
