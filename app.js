3e2bac27891b2125bb80f82e9d7cc4bad7dc1899
Pefes <pawlo898@gmail.com>
2020-05-27 18:52:58 +0200

    change port

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.use( tournamentRoutes );
 app.use( wrongRoutes );
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log( "Server is running on port: 3000" );
 });
